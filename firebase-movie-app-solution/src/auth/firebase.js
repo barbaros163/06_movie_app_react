861b9effb4f771c1b6a80f2abdafdf759aeccdec
Yasin Yavuz <barbaros1675@gmail.com>
2022-11-09 18:41:27 +0100

    v5

--- firebase-movie-app-solution/src/auth/firebase.js
+++ firebase-movie-app-solution/src/auth/firebase.js
@@ -5,6 +5,7 @@ import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
   signOut,
+  updateProfile,
 } from "firebase/auth";
 
 //* Your web app's Firebase configuration
@@ -26,7 +27,7 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export const createUser = async (email, password, navigate) => {
+export const createUser = async (email, password, navigate, displayName) => {
   //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
   try {
     let userCredential = await createUserWithEmailAndPassword(
@@ -34,6 +35,10 @@ export const createUser = async (email, password, navigate) => {
       email,
       password
     );
+    //? kullanici profilini güncellemek icin kullanilan firebase metodu
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    });
     navigate("/");
     console.log(userCredential);
   } catch (error) {
