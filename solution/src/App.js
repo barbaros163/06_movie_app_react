da4d4821bdd444d6e1695b3ed3f2cbb7cd35f8bc
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-08 00:53:22 +0100

    components added

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,9 +1,15 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import AuthContextProvider from "./context/AuthContextProvider";
 import AppRouter from "./router/AppRouter";
 
 const App = () => {
   return (
-    <div>
-      <AppRouter />
+    <div className="dark:bg-[#23242a]">
+      <AuthContextProvider>
+        <AppRouter />
+        <ToastContainer />
+      </AuthContextProvider>
     </div>
   );
 };
