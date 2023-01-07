7f75054e2e1cfdb9daefca1cce7175fe05740553
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-07 23:41:17 +0100

    some codes added

--- solution/src/helpers/ToastNotify.js
+++ solution/src/helpers/ToastNotify.js
@@ -1,7 +1,34 @@
-import React from "react";
+import { toast } from "react-toastify";
 
-const ToastNotify = () => {
-  return <div>ToastNotify</div>;
+export const toastWarnNotify = (msg) => {
+  toast.warn(msg, {
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
 };
 
-export default ToastNotify;
+export const toastSuccessNotify = (msg) => {
+  toast.success(msg, {
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+export const toastErrorNotify = (msg) => {
+  toast.error(msg, {
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
