2ef7a7ceb69ead7c5ebe005d8e06eeacaadef638
Yasin Yavuz <barbaros1675@gmail.com>
2023-01-09 17:00:13 +0100

    apis added

--- solution/src/pages/Main.jsx
+++ solution/src/pages/Main.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = (API) => {
+    axios
+      .get(API)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   return <div>Main</div>;
 };
 
